890fbb62698037b41a5be5024f7d4ea07d73afe9
abhijit <abhijit.pasupuleti@gmail.com>
2019-03-03 15:05:00 -0800

    change travis yml5

--- server/keys.js
+++ server/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  redisHost: process.env.REDIS_HOST,
+redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT,
   pgUser: process.env.PGUSER,
   pgHost: process.env.PGHOST,
